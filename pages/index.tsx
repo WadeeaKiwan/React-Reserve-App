6582785524c064cf2e6365d10e339ee486dcd44c
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-02-16 09:36:14 +0100

    Intro to Node + Express with API routs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
-const Home = () => <>Home</>;
+import React from "react";
+import axios from "axios";
+
+const Home = () => {
+  React.useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const url = "http://localhost:3000/api/products";
+    const response = await axios.get(url);
+    console.log(response.data);
+  };
+
+  return <>Home</>;
+};
 
 export default Home;
