79d53708dfad1f880191ce6d44a12f822f4b6053
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-02-16 09:54:52 +0100

    Fetching data on the server with getInitialProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import axios from "axios";
 
-const Home = () => {
-  React.useEffect(() => {
-    getProducts();
-  }, []);
+const Home = ({ products }) => {
+  console.log(products);
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
 
-  const getProducts = async () => {
-    const url = "http://localhost:3000/api/products";
-    const response = await axios.get(url);
-    console.log(response.data);
-  };
+  // const getProducts = async () => {
+  //   const url = "http://localhost:3000/api/products";
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // };
 
   return <>Home</>;
+};
+
+Home.getInitialProps = async () => {
+  // Fetch data on the server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  return {
+    products: response.data
+  };
+  // Return the response data as an object
+  // note: this object will be merged in existing props
 };
 
 export default Home;
