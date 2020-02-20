f91a1ad3e2b84d5c5118a638e6d024c0a428eb57
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-02-21 00:04:06 +0100

    Structure Cart Page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,3 +1,14 @@
-const Cart = () => <>Cart</>;
+import { Segment } from "semantic-ui-react";
+import CartItemList from "../components/Cart/CartItemList";
+import CartSummary from "../components/Cart/CartSummary";
+
+const Cart = () => {
+  return (
+    <Segment>
+      <CartItemList />
+      <CartSummary />
+    </Segment>
+  );
+};
 
 export default Cart;
