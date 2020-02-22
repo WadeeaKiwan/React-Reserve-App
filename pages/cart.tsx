604d616c617e3c79c2b9b5343550f3993c4baf53
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-02-23 00:57:22 +0100

    Fetch User Cart

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,14 +1,35 @@
 import { Segment } from "semantic-ui-react";
 import CartItemList from "../components/Cart/CartItemList";
 import CartSummary from "../components/Cart/CartSummary";
+import { parseCookies } from "nookies";
+import axios from "axios";
+import baseUrl from "../utils/baseUrl";
 
-const Cart = () => {
+const Cart = ({ products }) => {
+  console.log(products);
   return (
     <Segment>
       <CartItemList />
       <CartSummary />
     </Segment>
   );
+};
+
+Cart.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx);
+
+  // Check if the user is not authenticated
+  if (!token) {
+    return { products: [] };
+  }
+
+  const url = `${baseUrl}/api/cart`;
+  const payload = { headers: { Authorization: token } };
+  const response = await axios.get(url, payload);
+  console.log(response.data);
+  return {
+    products: response.data
+  };
 };
 
 export default Cart;
