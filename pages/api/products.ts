71d5d8143edbf3c3ad59f80f7855c3a8499e2bee
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-02-24 02:24:05 +0100

    Add Pagination for Product List

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -5,6 +5,27 @@ import connectDb from "../../utils/connectDb";
 connectDb();
 
 export default async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json(products);
+  const { page, size } = req.query;
+
+  // Convert querystring values to numbers
+  const pageNum = Number(page);
+  const pageSize = Number(size);
+  let products = [];
+
+  const totalDocs = await Product.countDocuments();
+  // Round if the rest are les than 'pageSize'
+  const totalPages = Math.ceil(totalDocs / pageSize);
+
+  // Display the first page
+  if (pageNum === 1) {
+    products = await Product.find().limit(pageSize);
+  } else {
+    // Escape the elements of previous pages
+    const skips = pageSize * (pageNum - 1);
+    products = await Product.find()
+      .skip(skips)
+      .limit(pageSize);
+  }
+
+  res.status(200).json({ products, totalPages });
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import axios from "axios";
 import ProductList from "../components/Index/ProductList";
+import ProductPagination from "../components/Index/ProductPagination";
 import baseUrl from "../utils/baseUrl";
 
-const Home = ({ products }) => {
-  return <ProductList products={products}></ProductList>;
+const Home = ({ products, totalPages }) => {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 };
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+  // Configure Pagination
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
   const url = `${baseUrl}/api/products`;
+  const payload = { params: { page, size } };
+
   // Fetch data on server
-  const response = await axios.get(url);
+  const response = await axios.get(url, payload);
+
   // Return the response data as an object
-  return {
-    products: response.data
-  };
+  return response.data;
   // note: this object will be merged in existing props
 };
 
