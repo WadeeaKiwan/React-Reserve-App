f8aa60e2662b6eac7ce8dedbffe12803ceecdc9b
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-02-26 01:15:06 +0100

    Add User Permissions

--- components/Account/AccountPermissions.tsx
+++ components/Account/AccountPermissions.tsx
@@ -16,11 +16,51 @@ const AccountPermissions = () => {
     const token = cookie.get("token");
     const payload = { headers: { Authorization: token } };
     const response = await axios.get(url, payload);
-    // setUsers(response.data);
+    setUsers(response.data);
     console.log(response.data);
   };
 
-  return <>AccountPermissions</>;
+  return (
+    <div style={{ margin: "2em 0" }}>
+      <Header as='h2'>
+        <Icon name='settings' />
+        User Permissions
+      </Header>
+      <Table compact celled definition>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Email</Table.HeaderCell>
+            <Table.HeaderCell>Joined</Table.HeaderCell>
+            <Table.HeaderCell>Updated</Table.HeaderCell>
+            <Table.HeaderCell>Role</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {users.map(user => (
+            <UserPermission key={user._id} user={user} />
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+};
+
+const UserPermission = ({ user }) => {
+  return (
+    <Table.Row>
+      <Table.Cell>
+        <Checkbox toggle />
+      </Table.Cell>
+      <Table.Cell>{user.name}</Table.Cell>
+      <Table.Cell>{user.email}</Table.Cell>
+      <Table.Cell>{user.createdAt}</Table.Cell>
+      <Table.Cell>{user.updatedAt}</Table.Cell>
+      <Table.Cell>{user.role}</Table.Cell>
+    </Table.Row>
+  );
 };
 
 export default AccountPermissions;
