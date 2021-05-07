5ec2525d397153091856e3550f0569729f320a04
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2021-05-07 11:17:24 +0200

    Update to vercel

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,6 +1,6 @@
 const baseUrl: string =
   process.env.NODE_ENV === "production"
-    ? "https://react-reserve-app.wadeeakiwan.now.sh"
+    ? "https://react-reserve-app.wadeeakiwan.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
